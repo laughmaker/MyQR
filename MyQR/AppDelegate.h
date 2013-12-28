e1a86a18328364a92722437252f4d0117b028f0b
makeLaugh <wskrdfeai@gmail.com>
2013-12-28 19:37:21 +0800

    Initial Commit

---
+++ MyQR/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyQR
+//
+//  Created by line0 on 13-12-28.
+//  Copyright (c) 2013年 ThreeWater. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
